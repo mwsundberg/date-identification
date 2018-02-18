6fb19a6848c7272e27c04d1f20adffd20b05e7a6
Claire MacCormick <cmaccorm@u.rochester.edu>
2018-02-18 02:46:14 -0500

    status bar wasn't gone on intro window, now it is

--- app/src/main/java/com/dandyhacks/datereader/PopUp.java
+++ app/src/main/java/com/dandyhacks/datereader/PopUp.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class PopUp extends AppCompatActivity {
@@ -32,6 +33,7 @@ public class PopUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         init();
 
 
